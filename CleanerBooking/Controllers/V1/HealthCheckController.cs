48fe4bd3b7215f8d268dbe4c488261df4cb4b427
ahmed.aboutaleb <ahmed.aboutaleb@talabat.com>
2024-02-02 13:06:01 +0200

    created a project inside the solution with health endpoint

---
+++ CleanerBooking/Controllers/V1/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanerBooking.Controllers.V1;
+
+[Route("api/health")]
+[ApiController]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new object());
+    }
+}
